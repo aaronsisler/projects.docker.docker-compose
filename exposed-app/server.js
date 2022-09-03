b224fcc6e6f9defa7f8987ae27e60729f8b07c9d
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2022-09-02 22:37:35 -0400

    Adding in internal comms portions

--- exposed-app/server.js
+++ exposed-app/server.js
@@ -7,12 +7,29 @@ const axios = require("axios").default;
 const EXPOSED_PORT = 8080;
 const PRIVATE_PORT = 9090;
 const HOST = "0.0.0.0";
+const PRIVATE_APP_HOST = process.env.PRIVATE_APP_URL || "private-app-url";
 
 // App
 const app = express();
 app.get("/", async (_req, res) => {
-  const internalResponse = await axios.get(`http://${HOST}:${PRIVATE_PORT}`);
-  res.send(internalResponse.data);
+  try {
+    res.send("Silly endpoint");
+  } catch (e) {
+    console.log(e);
+    res.send(e);
+  }
+});
+app.get("/call-internal", async (_req, res) => {
+  try {
+    console.log(PRIVATE_APP_HOST);
+    const internalResponse = await axios.get(
+      `http://${PRIVATE_APP_HOST}:${PRIVATE_PORT}`
+    );
+    res.send(internalResponse.data + " A little something exposed...");
+  } catch (e) {
+    console.log(e);
+    res.send(e);
+  }
 });
 
 app.listen(EXPOSED_PORT, HOST);
